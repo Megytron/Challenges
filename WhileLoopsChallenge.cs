245b1c136431aa4d9db325668d80574e44be68b9
Megyn Pettry <105400607+Megytron@users.noreply.github.com>
2022-06-09 22:35:29 -0600

    While Loops Challenge

---
+++ WhileLoopsChallenge.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main(string[] args)
+    {
+      int i = 100;
+      do 
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+      while (i < 125);
+    }
+  }
